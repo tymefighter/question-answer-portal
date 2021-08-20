70e2df62b13db02e354d18ea8380a0f042f55215
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-08-20 17:48:13 +0530

    SERVER feat: updates login response to be JSON with a role property instead of just a message

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -1,17 +1,18 @@
 import * as express from 'express';
-import { authenticateAndAuthorize } from './helper';
+import { authenticateAndAuthorize, RequestWithUserInfo } from './helper';
 
 import Database from '../database';
+import { Role } from '../database/types';
 
 export default function addLoginRoute(database: Database, app: express.Express) {
   app
     .route('/login')
     .post(
       authenticateAndAuthorize(database, ['STUDENT', 'SETTER', 'EVALUATOR']), 
-      (req, res) => {
+      (req: RequestWithUserInfo, res) => {
         res
           .status(200)
-          .end('Login Successful');
+          .json({ role: (req.user?.role as Role) });
       }
     );
 }
