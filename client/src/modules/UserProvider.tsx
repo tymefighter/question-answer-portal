b5a703d4856d30241d3083c4aa60637f309990d4
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2021-08-21 12:35:05 +0530

    CLIENT feat: adds user provider

---
+++ client/src/modules/UserProvider.tsx
@@ -0,0 +1,35 @@
+// Library
+import React from 'react';
+
+// Components
+import UserContext from 'context/UserContext';
+
+// Types
+import { User } from 'types';
+import { useEffect } from 'react';
+
+interface UserProviderProps {
+  children: React.ReactElement;
+};
+
+export default function UserProvider({ children }: UserProviderProps) {
+
+  const [user, setUser] = React.useState<User | undefined>();
+
+  useEffect(() => {
+    const userJSON = sessionStorage.getItem('user');
+
+    if(userJSON) setUser(JSON.parse(userJSON) as User);
+  }, []);
+
+  const setUserAlongWithSessionStorage = (user : User) => {
+    setUser(user);
+    sessionStorage.setItem('user', JSON.stringify(user));
+  } 
+
+  return (
+    <UserContext.Provider value={{ user, setUser: setUserAlongWithSessionStorage }}>
+      { children }
+    </UserContext.Provider>
+  );
+}
